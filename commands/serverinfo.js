09762f84f3d74eac05f16dfe113df2262a7af81a
Glitch (chestnut-moored-spatula) <none>
2023-01-25 17:08:36 +0000

    ☔️🌚 Checkpoint ./commands/serverinfo.js:53392521/416

---
+++ commands/serverinfo.js
@@ -0,0 +1,11 @@
+const { messageEmbed } = require('discord.js');
+
+module.exports = {
+  info: {
+    name: "serverinfo",
+    description: "to get server information",
+    usage: "[serverinfo]",
+    aliases: ["svi"],
+  }}
+
+messageEmbed.setColor()
