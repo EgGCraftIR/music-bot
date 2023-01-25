ee0435c17d00c1eb83df15c10611950d12cdd6aa
Glitch (chestnut-moored-spatula) <none>
2023-01-25 18:19:35 +0000

    🍐🚞 Checkpoint ./commands/invite.js:53392521/309

--- commands/invite.js
+++ commands/invite.js
@@ -15,9 +15,10 @@ module.exports = {
     
     let invite = new MessageEmbed()
     .setTitle(`Invite ${client.user.username}`)
-    .setDescription(`Want me in your server? Invite me today! \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)`)
+    .setDescription(`منو واسه سرورت میخوای ؟ پس چرا اینوایت نمیدی ؟ \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)`)
     .setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot`)
-    .setColor("BLUE")
+    .setColor("BROWN")
+    .setImage('https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1200px-Heart_coraz%C3%B3n.svg.png')
     return message.channel.send(invite);
   },
 };
