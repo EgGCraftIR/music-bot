e481cad9113a0e9c202c834bf7672f70b248835b
Glitch (chestnut-moored-spatula) <none>
2023-01-26 14:16:05 +0000

    🚩💸 Checkpoint ./events/ready.js:53392521/310

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,6 @@
 module.exports = async (client) => {
-   var allcmds = ""
-               
-
    function YousamPower() {
-      let hungry = [""]
+      let hungry = ["!help", "!about", "!afk", "!discord", "!invite", "!loop", "!lyrics", "!nowplaying", "!pause", "!remove", "!resume", "!search", "!shuffle", "!skip"]
       let Power = Math.floor(Math.random() * hungry.length);
       client.user.setActivity(hungry[Power], {type: "STREAMING", url: "https://www.twitch.tv/shabake4"});
     }; setInterval(YousamPower, 3000)
