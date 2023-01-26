8b242e0974a194191ca46c55bbe9899fe6d4a61d
Glitch (chestnut-moored-spatula) <none>
2023-01-26 13:28:11 +0000

    🐵🎆 Checkpoint ./commands/welcomer.js:53392521/4796

--- commands/welcomer.js
+++ commands/welcomer.js
@@ -4,35 +4,20 @@ const { MessageEmbed, MessageWelcome, Client } = require("discord.js");
 module.exports = {
   info: {
     name: "welcomer",
-    description: "setup  welcome embed channel",
-    usage: "channel",
+    description: "create welcome embed",
+    usage: "[channel]",
     aliases: ["w"],
   },
-run: async client.on('guildMemberAdd', member => {
-    let welcomeChannel = client.channel.selected
-    let targetchannel = selectedChannel
-    if(welcomeChannel){
-
-        let welcomeEmbed = new discord.MessageEmbed()
-        if(member.user.bot){
-
-        if(member.user.client){
-
-            welcomeEmbed.setColor("YELLOW")
-            welcomeEmbed.setAuthor(`${member.guild.name}`)
-            welcomeEmbed.setDescription(`سلام <@${member.user.id}> به سرور ما خوش اومدی حتما یه سر بزن به ${member.guild.channels.cache.get(targetchannel).toString()} امیدوارم از سرورمون خوشت بیاد`)
-            welcomeEmbed.setThumbnail(member.user.displayAvatarURL())
-            welcomeEmbed.setFooter(`${client.user.username} (=`)
-            welcomeChannel.send(welcomeEmbed)
-        }}else{
-            welcomeEmbed.setColor('YELLOW')
-            welcomeEmbed.setAuthor(`${member.guild.name}`)
-            welcomeEmbed.setDescription(`سلام <@${member.user.id}> به سرور ما خوش اومدی حتما یه سر بزن به ${member.guild.channels.cache.get(targetchannel).toString()} امیدوارم از سرورمون خوشت بیاد`)
-            welcomeEmbed.setThumbnail(member.user.displayAvatarURL())
-            welcomeEmbed.setFooter(`${client.user.username}  (=  `)
-            welcomeChannel.send(welcomeEmbed)
-        }
-    }else{
-        console.log("Welcome Channel Yaft Nashod")
-    }
-  })
+run: async function (client, message, args) {
+    
+    //set the permissions id here (https://discordapi.com/permissions.html)
+    var permissions = 4398046511095;
+    
+    let welcomer = new MessageEmbed()
+    .setColor("YELLOW")
+    .setAuthor('PersianCat Discord Server Link')
+    .setFooter('thank you for using me')
+    .setDescription('https://discord.gg/frVDXRAs')
+    return message.channel.send(welcomer);
+  },
+};
