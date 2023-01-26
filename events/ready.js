e5d6acce28d8900aaed190587ddd1621f5abc14c
Glitch (chestnut-moored-spatula) <none>
2023-01-26 16:24:27 +0000

    👙⛲️ Checkpoint ./events/ready.js:53392521/214

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,7 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
   client.guilds.cache.get("1025711153080434688")
-  await client.user.setActivity(`${client.ServersCount} Servers`, {
+  await client.user.setActivity(`!help `, {
     type: "WATCHING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
