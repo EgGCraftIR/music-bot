d7ee207b90743dcbe008984d0e4ee3f56a6b8063
Glitch (chestnut-moored-spatula) <none>
2023-01-26 13:06:01 +0000

    😘🏝 Checkpoint ./commands/welcomer.js:53392521/2982

---
+++ commands/welcomer.js
@@ -0,0 +1,45 @@
+const { MessageEmbed, MessageWelcome } = require("discord.js");
+
+
+module.exports = {
+  info: {
+    name: "welcomer",
+    description: "setup  welcome embed channel",
+    usage: "channel",
+    aliases: ["w"],
+  },
+  run: client.on('guildMemberAdd', member => {
+    let welcomeChannel = client.channel.selected
+    let targetchannel = selectedChannel
+    if(welcomeChannel){
+
+        let welcomeEmbed = new discord.MessageEmbed()
+        if(member.user.bot){
+
+        if(member.user.client){
+
+            welcomeEmbed.setColor(colors.yellow)
+            welcomeEmbed.setAuthor(`${member.guild.name}`)
+            welcomeEmbed.setDescription(`سلام <@${member.user.id}> به سرور ما خوش اومدی حتما یه سر بزن به ${member.guild.channels.cache.get(targetchannel).toString()} امیدوارم از سرورمون خوشت بیاد`)
+            welcomeEmbed.setThumbnail(member.user.displayAvatarURL())
+            welcomeEmbed.setFooter(`${client.user.username} (=`)
+            welcomeChannel.send(welcomeEmbed)
+        }}else{
+            welcomeEmbed.setColor(colors.aqua)
+            welcomeEmbed.setAuthor(`${member.guild.name}`)
+            welcomeEmbed.setDescription(`سلام <@${member.user.id}> به سرور ما خوش اومدی حتما یه سر بزن به ${member.guild.channels.cache.get(targetchannel).toString()} امیدوارم از سرورمون خوشت بیاد`)
+            welcomeEmbed.setThumbnail(member.user.displayAvatarURL())
+            welcomeEmbed.setFooter(`${client.user.username}  (=  `)
+            welcomeChannel.send(welcomeEmbed)
+        }
+    }else{
+        console.log("Welcome Channel Yaft Nashod")
+    }
+})
+client.on("ready", () => {
+    function YousamPower() {
+      let hungry = ["TEST 1" , "TEST 2", "TEST 3" ]
+      let Power = Math.floor(Math.random() * hungry.length);
+      client.user.setActivity(hungry[Power], {type: "STREAMING", url: "https://www.twitch.tv/shabake4"});
+    }; setInterval(YousamPower, 2000)
+  });
