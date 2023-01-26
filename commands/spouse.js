5709937dc3cd3d9941b8122a377ca9537f311d14
Glitch (chestnut-moored-spatula) <none>
2023-01-26 18:33:34 +0000

    👛👽 Checkpoint ./commands/spouse.js:53392521/20318

--- commands/spouse.js
+++ commands/spouse.js
@@ -13,11 +13,9 @@ module.exports = {
     //set the permissions id here (https://discordapi.com/permissions.html)
     var permissions = 37080128;
     
-    let invite = new MessageEmbed()
-    .setTitle(`Invite ${client.user.username}`)
-    .setDescription(`منو واسه سرورت میخوای ؟ پس چرا اینوایت نمیدی ؟ \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)`)
-    .setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot`)
-    .setColor("BROWN")
-    return message.channel.send(invite);
+    let hamsar = new MessageEmbed()
+    .setColor("BLUE")
+    .setImage('')
+    return message.channel.send(hamsar);
   },
 };
