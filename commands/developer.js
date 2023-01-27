26664037ad8df0c6242ec51b63f1f4a3a42db52d
Glitch (chestnut-moored-spatula) <none>
2023-01-27 13:44:49 +0000

    🕍🏈 Checkpoint ./commands/developer.js:53392521/791

---
+++ commands/developer.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  info: {
+    name: "developer",
+    description: "To see ",
+    usage: "",
+    aliases: ["ab"],
+  },
+
+  run: async function (client, message, args) {
+    
+    //set the permissions id here (https://discordapi.com/permissions.html)
+    var permissions = 0;
+    
+    let about = new MessageEmbed()
+    .setColor("YELLOW")
+    .setDescription('Empire Cat is a Discord server that has 2 admins and 1 founder that has a bot named Persian Cat that is completely Iranian and is for entertainment and other things that will be added soon.')
+    .setAuthor('Thank you for using me')
+    .setFooter('Written by just_amirHeHe#0485')
+    return message.channel.send(about);
+  },
+};
