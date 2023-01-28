a0e2e0c46233dedf761fa99e358b6b8dfb885faa
Glitch (chestnut-moored-spatula) <none>
2023-01-28 17:22:59 +0000

    🎨🐡 Checkpoint ./commands/ComigSoon.js:53392521/521

---
+++ commands/ComigSoon.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  info: {
+    name: "comingsoon",
+    description: "new command",
+    usage: "",
+    aliases: "cs"
+  },
+  
+  run: async function ( message, client, args ) {
+  
+  var premission = 0;
+  
+  let comingsoon = new messageEmbed()
+  .setColor("YELLOW")
+  .setDescription("comingsoon")
+  .setAuthor('comingsoon')
+  .s
+ },
+};
