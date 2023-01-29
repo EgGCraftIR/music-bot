3b9cbc9c09ad5654b65f28eddef3dd25bdef4c89
Glitch (chestnut-moored-spatula) <none>
2023-01-29 12:24:27 +0000

    🚓😋 Checkpoint ./commands/help.js:53392521/846

--- commands/help.js
+++ commands/help.js
@@ -21,6 +21,7 @@ module.exports = {
         .setDescription(allcmds)
         .setFooter(`اگه دیدید کامندی کار نمیکنه بدونین درحال ساخته . در صورت دریافت مشکل میتوانید به سرور دیسکورد ما  جوین بدید :https://discord.gg/frVDXRAs`)
         .setAuthor('❤با تشکر از اینکه بات ما رو به سرورتون اینوایت دادید. با این کار مارو به وریفای نزدیک میکنید')
+        .setImage('https://images.macrumors.com/t/hi1_a2IdFGRGMsJ0x31SdD_IcRk=/1600x/article-new/2018/05/apple-music-note.jpg')
         if(!args[0])return message.channel.send(embed)
         else {
             let cmd = args[0]
