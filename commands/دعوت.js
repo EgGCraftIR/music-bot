0860ca85162ffc50d844ea8ecd74c997012c3051
Glitch (chestnut-moored-spatula) <none>
2023-02-02 10:45:50 +0000

    🐡🍥 Checkpoint ./.glitch-assets:53392521/1168 ./commands/دعوت.js:53392521/135 ./commands/سازنده.js:53392521/136

--- commands/دعوت.js
+++ commands/دعوت.js
@@ -18,6 +18,7 @@ module.exports = {
     .setDescription(`منو واسه سرورت میخوای ؟ پس چرا اینوایت نمیدی ؟ \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot)`)
     .setURL(`https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=${permissions}&scope=bot`)
     .setColor("BROWN")
+    .setImage('https://cdn.glitch.global/1aeb3279-ad5b-4159-bd08-aafdd1fd0f22/invite.gif?v=1675334358610')
     return message.channel.send(invite);
   },
 };
--- commands/سازنده.js
+++ commands/سازنده.js
@@ -17,6 +17,7 @@ module.exports = {
     .setColor("YELLOW")
     .setAuthor("just_amirHeHe#0485")
     .setDescription("اشکالات بات را در دی ام من گزارش بدید")
+    .setImage('https://cdn.glitch.global/1aeb3279-ad5b-4159-bd08-aafdd1fd0f22/just_amir.gif?v=1675334543921')
     return message.channel.send(dev);
   },
 };
