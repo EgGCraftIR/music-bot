1a5dce90097df768916b687fbb2c1c7ea2b03370
Glitch (chestnut-moored-spatula) <none>
2023-02-03 12:55:03 +0000

    🕶🚨 Checkpoint ./commands/فحش.js:53392521/1847

---
+++ commands/فحش.js
@@ -0,0 +1,21 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  info: {
+    name: "فحش",
+    description: "برای دیدن دستورات زد فحش بات",
+    usage: "",
+    aliases: ["فحش"],
+  },
+
+  run: async function (client, message, args) {
+    
+    //set the permissions id here (https://discordapi.com/permissions.html)
+    var permissions = 0;
+    
+    let abbas = new MessageEmbed()
+    .addField('!addword',"اضافه کردن کلمه به فهرست سیاه")
+    .addField('!delword',"حذف کلمه از فهرست سیاه")
+    return message.channel.send(abbas);
+  },
+};
