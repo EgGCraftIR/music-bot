4d59ebc9671a57f8ff98dfbc8c8232ad2c8d89a4
Glitch (chestnut-moored-spatula) <none>
2023-02-04 15:13:44 +0000

    🎡🦀 Checkpoint ./commands/بوس.js:53392521/518 ./.glitch-assets:53392521/410

--- commands/بوس.js
+++ commands/بوس.js
@@ -1,10 +1,11 @@
-const { MessageEmbed, MentionUser } = require("discord.js");
+const { MessageEmbed } = require("discord.js")
+
 
 module.exports = {
   info: {
     name: "بوس",
-    description: "برای بوسیدن یک فرد",
-    usage: "فرد",
+    description: "برای بوسیدن بات",
+    usage: "",
     aliases: ["بو"],
   },
 
@@ -14,7 +15,8 @@ module.exports = {
     var permissions = 37080128;
     
     let invite = new MessageEmbed()
-    .setAuthor(`@${message.author.tag} بوسید ${MentionUser}`)
+    .setAuthor(`@${message.author.tag} بوسید ${client.user.tag}`)
+    .setImage('https://cdn.glitch.global/1aeb3279-ad5b-4159-bd08-aafdd1fd0f22/4955-flushed-smallface.png?v=1675523304965')
     return message.channel.send(invite);
   },
 };
