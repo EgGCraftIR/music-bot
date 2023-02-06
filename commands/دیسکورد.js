bf0f7d28a4374b97eea2c6b530388ce5b06c732d
Glitch (chestnut-moored-spatula) <none>
2023-02-06 21:41:27 +0000

    🍴🕋 Checkpoint ./commands/دیسکورد.js:53392521/263

--- commands/دیسکورد.js
+++ commands/دیسکورد.js
@@ -12,7 +12,12 @@ module.exports = {
     
     //set the permissions id here (https://discordapi.com/permissions.html)
     var permissions = 0;
+    let embed = new MessageEmbed()
+    .setAuthor('سرور پشتیبانی پرشین کت')
+    .setDescription('هرگونه مشکل بات را در سرور به ادمین ها گزارش دهید')
+    .setTimestamp()
+    .setFooter('ممنون از شما')
     
-   message.channel.send('https://discord.gg/aGBbYvesaT')
+   message.channel.send('https://discord.gg/aGBbYvesaT', embed)
   },
 };
