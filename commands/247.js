4a424cbabcd3ce02bfc7055a1e4615ba745e1fa4
Glitch (chestnut-moored-spatula) <none>
2023-02-07 20:11:17 +0000

    ❄️🕋 Checkpoint ./commands/247.js:53392521/1027

---
+++ commands/247.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require("discord.js");
+const sendError = require("../util/error");
+const fs = require('fs');
+
+
+module.exports = {
+  info: {
+    name: "247",
+    description: "24/7",
+    usage: "[ای اف کی]",
+    aliases: ["afk"],
+  },
+
+  run: async function (client, message, args) {
+    let afk = JSON.parse(fs.readFileSync("./afk.json", "utf8"));
+       if (!afk[message.guild.id]) afk[message.guild.id] = {
+        afk: false,
+    };
+    var serverQueue = afk[message.guild.id]
+       if (serverQueue) {
+            serverQueue.afk = !serverQueue.afk;
+             message.channel.send({
+                embed: {
+                    color: "GREEN",
+                    description: `💤  **|**  ای اف کی **\`${serverQueue.afk === true ? "فعال شد" : "غبر فعال شد"}\`**`
+                }
+            });
+            return  fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {
+        if (err) console.error(err);
+    });
+        };
+    return sendError("چیزی در این سرور پخش نمی شود.", message.channel);
+  },
+};
