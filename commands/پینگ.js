913ede6455ae653e32cc8142509ba80202799040
Glitch (chestnut-moored-spatula) <none>
2023-02-09 16:15:14 +0000

    🌪⛈ Checkpoint ./commands/پینگ.js:53392521/980

---
+++ commands/پینگ.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require("discord.js");
+const sendError = require("../util/error");
+
+module.exports = {
+  info: {
+    name: "پینگ",
+    description: "برای دیدن امار بات",
+    usage: "",
+    aliases: [],
+  },
+
+  run: async function (client, message, args) {
+      let xd = new MessageEmbed()
+      .setDescription(`**پینگ بات:** ${client.ws.ping} | **سرور های درحال مشاهده:** ${client.guilds}`)
+      .setTimestamp()
+      return message.channel.send(xd);
+  },
+};
