942b037f1543b9e98396703383212ee05ef39871
Glitch (chestnut-moored-spatula) <none>
2023-02-10 13:08:52 +0000

    🙈🎐 Checkpoint ./index.js:53392521/2867

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();//Loading .env
 const fs = require("fs");
+const MessageEmbed = require("discord.js");
 const { Collection, Client } = require("discord.js");
 
 const client = new Client();//Making a discord bot client
@@ -32,6 +33,7 @@ fs.readdir("./commands/", (err, files) => {
     console.log("Loading Command: "+commandName)
   });
 });
+
 //auto pinging
 
 //thats it
